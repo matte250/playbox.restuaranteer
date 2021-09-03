5ccdf0a069181dc07b46ff45d0a83ec7ca0d8f64
Mattias Soroccos <mattias.soroccos@inet.se>
2021-09-03 11:37:34 +0200

    Create user data model

--- sql/init.sql
+++ sql/init.sql
@@ -1,2 +1,11 @@
     DROP DATABASE IF EXISTS restu_db;
     CREATE DATABASE restu_db; 
+
+    USE restu_db;
+
+    CREATE TABLE users(
+        id VARCHAR(36)  -- guid
+        email VARCHAR(320) -- Maxmimum length of an email address defined by RFC 3696
+        [password] VARCHAR(120)
+        [name] varchar(40)
+    )
