e3c15aff19f444ffe7178e579945e411d2130c4e
Mattias S <matte_1500@hotmail.com>
2021-09-21 23:37:48 +0000

    Fix init.sql

--- sql/init.sql
+++ sql/init.sql
@@ -14,7 +14,7 @@ INSERT INTO users (id, email, password, name) VALUES("19a5e8cd-edab-40ce-82ea-d4
 
 CREATE TABLE places(
     id VARCHAR(36) NOT NULL PRIMARY KEY,
-    name VARCHAR(40) NOT NULL,
+    `name` VARCHAR(40) NOT NULL,
     createdBy VARCHAR(36) NOT NULL,  
     googleMapsLink VARCHAR(2000), -- Meant for links, value is quite arbitary.
     FOREIGN KEY (createdBy) REFERENCES users(id)
@@ -37,6 +37,14 @@ CREATE TABLE experiences(
     FOREIGN KEY (createdBy) REFERENCES users(id)
 );
 
+INSERT INTO experiences (id, at, `when`, createdBy) 
+VALUES(
+    "1efa309f-6995-4dc4-92fd-05af070d99fb",
+    "37ad7834-e117-42d1-bf6c-760fff93c2e8",
+    "2021-09-21 22:53:00",
+    "19a5e8cd-edab-40ce-82ea-d4420ec6873a"
+);
+
 CREATE TABLE reviews(
     id VARCHAR(36) NOT NULL PRIMARY KEY,
     createdBy VARCHAR(36) NOT NULL,
@@ -46,5 +54,15 @@ CREATE TABLE reviews(
     mealName VARCHAR(40) NOT NULL, -- Arbitrary length
     note TEXT, -- Maybe use VARCHAR here? Need to read up on MySql data types
     FOREIGN KEY (createdBy) REFERENCES users(id),
-    FOREIGN KEY (experience) REFERENCES experiences(id),
+    FOREIGN KEY (experience) REFERENCES experiences(id)
+);
+
+INSERT INTO reviews VALUES(
+    "d2cae516-06d4-4fd6-bfde-d3f92c115481",
+    "19a5e8cd-edab-40ce-82ea-d4420ec6873a",
+    "0.3",
+    "2021-09-21 22:52:00",
+    "1efa309f-6995-4dc4-92fd-05af070d99fb",
+    "Chicken Kiev",
+    "I have had better."
 );
