723c185e97e607b091ab80203bcd942f073eff68
Mattias S <matte_1500@hotmail.com>
2021-11-11 18:49:09 +0000

    Update runTests file

--- runTests.sh
+++ runTests.sh
@@ -1,3 +1,3 @@
 echo "Starting tests"
 wget -q -nc -O karate.jar https://github.com/karatelabs/karate/releases/download/v1.2.0.RC1/karate-1.2.0.RC1.jar
-java -Dconf.baseurl='http://localhost:3000' -jar ./karate.jar -cp ./ ../
+java -Dconf.baseurl='http://localhost:3000' -jar ./karate.jar ./
